1dd6fdd3f67b0d13ced01ceab69181650762e07a
sarahaliyah <dayangkusarahnuurulaliyah.sarahaliyah@my.jcu.edu.au>
2025-02-06 16:34:36 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,15 +2,18 @@ PASSWORD_LENGTH = 10
 
 
 def main():
+    """get and print password using function"""
     user_password = get_password()
     print_asterisks(user_password)
 
 
 def print_asterisks(user_password):
+    """print asterisks as the length of characters in user's password"""
     print(user_password * "*")
 
 
 def get_password():
+    """get user's password meeting the required characters"""
     user_password = len(input("Enter your password: "))
     while user_password <= PASSWORD_LENGTH:
         print("Invalid Password")
