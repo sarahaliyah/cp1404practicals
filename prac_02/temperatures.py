53d63b7dc675cacbd67d97de7a575dd293aa53c7
sarahaliyah <dayangkusarahnuurulaliyah.sarahaliyah@my.jcu.edu.au>
2025-02-06 15:58:52 +0800

    Refactor temperature conversion program to use functions

---
+++ prac_02/temperatures.py
@@ -0,0 +1,43 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+
+
+def main():
+    """convert user input from one temperature unit to another"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            # Convert Fahrenheit to Celsius and display the result
+            fahrenheit = float(input("Fahrenheit: "))
+            # Hint: celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """convert fahrenheit to celsius"""
+    return 5 / 9 * (fahrenheit - 32)
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    """convert celsius to fahrenheit"""
+    return celsius * 9.0 / 5 + 32
+
+
+main()
