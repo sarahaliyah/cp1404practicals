35e071e45805510410a6c247f243ddaf3cb24634
sarahaliyah <dayangkusarahnuurulaliyah.sarahaliyah@my.jcu.edu.au>
2025-03-04 14:44:13 +0800

    Add program to process Wimbledon champions and winning countries from CSV file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,14 +9,14 @@ INDEX_CHAMPION = 2
 
 
 def main():
-    """Read the data file and display the processed data of Wimbledon champions and countries."""
+    """Read the data file and display the processed data of Wimbledon champions and countries"""
     data = load_data(FILENAME)
     champion_counts, countries = process_data(data)
     display_results(champion_counts, countries)
 
 
 def display_results(champion_counts, countries):
-    """Display the results of champion wins and countries."""
+    """Display the results of champion wins and countries"""
     print("Wimbledon Champions: ")
     for champion, count in champion_counts.items():
         print(f"{champion} {count}")
@@ -27,7 +27,7 @@ def display_results(champion_counts, countries):
 
 
 def process_data(data):
-    """Create a dictionary and get champions count and their countries."""
+    """Create a dictionary and get champions count and their countries"""
     champion_counts = {}
     countries = set()
     for row in data:
@@ -40,7 +40,7 @@ def process_data(data):
 
 
 def load_data(filename):
-    """Load the data from the file in list of lists form."""
+    """Load the data from the file in list of lists form"""
     data = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline().strip().split(',')
