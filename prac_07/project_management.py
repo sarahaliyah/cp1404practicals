72f6ef8558e98838c51982d986a394a2563bc1f9
sarahaliyah <dayangkusarahnuurulaliyah.sarahaliyah@my.jcu.edu.au>
2025-03-24 01:56:36 +0800

    Implement main function for project management with user interactions and menu options

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -16,4 +16,39 @@ MENU = ("- (L)oad projects \n"
         "- (Q)uit")
 
 
+def main():
+    """Program for a project management that track project completion and project details that user wish to complete."""
+    projects = load_projects(FILENAME)
+    print("Welcome to Pythonic Project Management")
+    print(f"Load {len(projects)} projects from {FILENAME}")
+    print(MENU)
+
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+           filename = input("Filename: ")
+           projects = load_projects(filename)
+        elif choice == "S":
+            filename = input("Filename: ")
+            save_projects(filename, projects)
+        elif choice == "D":
+            display_projects(projects)
+        elif choice == "F":
+            date = input("Show projects that start after date (dd/mm/yyyy): ")
+            filter_projects_by_date(projects, date)
+        elif choice == "A":
+            add_project(projects)
+        elif choice == "U":
+            update_project(projects)
+        else:
+            print("Invalid menu choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    save_choice = input(f"Would you like to save to {FILENAME}? ").strip().lower()
+    if save_choice in ["yes", "y"]:
+        save_projects(FILENAME, projects)
+        print(f"Projects saved to {FILENAME}.")
+    else:
+        print("No changes were saved.")
+    print("Thank you for using custom-built project management software.")
 
