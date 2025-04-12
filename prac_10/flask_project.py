c15376ac26ae9e31c9293a0ba9086501e683dc44
sarahaliyah <dayangkusarahnuurulaliyah.sarahaliyah@my.jcu.edu.au>
2025-04-12 20:59:38 +0800

    Add /greet route with optional name parameter

--- prac_10/flask_project.py
+++ prac_10/flask_project.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello World :) </h1>'
 
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
 
 if __name__ == '__main__':
     app.run()
