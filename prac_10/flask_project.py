7961057ae05619a4f44ca64e8910c0eb54df3bf6
sarahaliyah <dayangkusarahnuurulaliyah.sarahaliyah@my.jcu.edu.au>
2025-04-12 21:03:39 +0800

    Added Fahrenheit to Celsius conversion at /c/<fahrenheit> route

--- prac_10/flask_project.py
+++ prac_10/flask_project.py
@@ -12,5 +12,11 @@ def hello_world():
 def greet(name=""):
     return f"Hello {name}"
 
+@app.route('/c/<fahrenheit>')
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert entered temperature from Fahrenheit to Celsius"""
+    celsius = 5 / 9 * (float(fahrenheit) - 32)
+    return f"Input fahrenheit value:{fahrenheit}  -  Output: {celsius:.2f}"
+
 if __name__ == '__main__':
     app.run()
